5c989ffea50be19bcb3c959f9647f46e8ac765da
David <oliveiradavid.dev@gmail.com>
2024-05-03 13:44:56 -0400

    chore: start project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { mapActions, mapState } from 'pinia';
+import { defineComponent } from 'vue';
+import { useCounterStore } from '@/stores/counter';
+
+export default defineComponent({
+  components: { },
+  setup() {
+    const counterStore = useCounterStore()
+
+    return { counterStore }
+  },
+  methods: {
+    incrementAndPrint() {
+      this.counterStore.increment()
+      console.log('New Count:', this.counterStore.count)
+    },
+  },
+})
+
+</script>
+<template>
+  <main>
+    {{  counterStore.count  }}
+    <button @click="incrementAndPrint">Add</button>
+  </main>
+</template>
